7e87b2181f70317dfddad4cc55110aaee384984c
Valeri_L <idval@imedia.ru>
2023-01-29 23:54:55 +0300

    Last moment minor fixings

--- FirstTermTest/MyLibrary.cs
+++ FirstTermTest/MyLibrary.cs
@@ -1643,15 +1643,5 @@ string lline;
             }  
         }
         return true;
-// string line;
-//       Console.WriteLine("Enter one or more lines of text (press CTRL+Z to exit):");
-//       Console.WriteLine();
-//       do {
-//          Console.Write("   ");
-//          line = Console.ReadLine();
-//          if (line != null)
-//             Console.WriteLine("      " + line);
-//       } while (line != null);
-
     }
 }
